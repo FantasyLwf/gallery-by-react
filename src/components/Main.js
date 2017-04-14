fd8166635f706a6af1313c4d71b4ecf1bfce799b
liweifengstu <liweifengstu@gmail.com>
2017-04-14 22:15:23 +0800

    init project structrue

---
+++ src/components/Main.js
@@ -0,0 +1,35 @@
+require('normalize.css/normalize.css');
+require('styles/App.scss');
+
+import React from 'react';
+
+let imageDatas = require('../data/imageDatas.json');
+
+imageDatas=(function genImageURL(imageDatasArr){
+    for (let i=0;i<imageDatasArr.length;i++){
+        let singleImageData=imageDatasArr[i];
+        singleImageData.imageURL=require('../images/'+singleImageData.filename);
+        imageDatasArr[i]=singleImageData;
+    }
+    return imageDatasArr;
+})(imageDatas);
+
+class AppComponent extends React.Component {
+    render() {
+        return (
+            <div> </div>
+            /*<section className="stage">
+                <section className="img-sec">
+
+                </section>
+                /*<nav className="controller-nav">
+
+                </nav>
+            </section>*/
+        );
+    }
+}
+
+AppComponent.defaultProps = {};
+
+export default AppComponent;
