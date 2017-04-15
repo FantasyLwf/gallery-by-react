43cc82fd2d501cf65a48e0e41665156fbbd7ed8d
liweifengstu <liweifengstu@gmail.com>
2017-04-16 00:06:23 +0800

    change 4.16

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,30 +5,50 @@ import React from 'react';
 
 let imageDatas = require('../data/imageDatas.json');
 
-imageDatas=(function genImageURL(imageDatasArr){
-    for (let i=0;i<imageDatasArr.length;i++){
-        let singleImageData=imageDatasArr[i];
-        singleImageData.imageURL=require('../images/'+singleImageData.filename);
-        imageDatasArr[i]=singleImageData;
+imageDatas = (function genImageURL(imageDatasArr) {
+    for (let i = 0; i < imageDatasArr.length; i++) {
+        let singleImageData = imageDatasArr[i];
+        singleImageData.imageURL = require('../images/' + singleImageData.filename);
+        imageDatasArr[i] = singleImageData;
     }
     return imageDatasArr;
 })(imageDatas);
 
-class AppComponent extends React.Component {
+class ImgFigure extends React.component {
     render() {
         return (
-            <section className="stage">
-                <section className="img-sec">
+            <figure>
+                <img src = {this.props.data.imageURL} alt = {this.props.data.title}/>
+                <figcaption>
+                    <h2>{this.props.data.title}</h2>
+                </figcaption>
+            </figure>
+        );
+    }
+}
 
+class GalleryByReactApp extends React.Component {
+    render() {
+        let controllerUnits = [];
+        let imgFigures = [];
+
+        imageDatas.forEach(function (value) {
+            imgFigures.push(<ImgFigure data = {value}/>);
+        });
+
+        return (
+            <section className = "stage">
+                <section className = "img-sec">
+                    {imgFigures}
                 </section>
-                <nav className="controller-nav">
-
+                <nav className = "controller-nav">
+                    {controllerUnits}
                 </nav>
             </section>
         );
     }
 }
 
-AppComponent.defaultProps = {};
+GalleryByReactApp.defaultProps = {};
 
-export default AppComponent;
+export default GalleryByReactApp;
