a7d8bc25c01d375ddd967e077233bddf08ee10a7
liweifengstu <liweifengstu@gmail.com>
2017-04-16 00:40:53 +0800

    change 4.16

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,10 +17,10 @@ imageDatas = (function genImageURL(imageDatasArr) {
 class ImgFigure extends React.component {
     render() {
         return (
-            <figure>
+            <figure className="img-figure">
                 <img src = {this.props.data.imageURL} alt = {this.props.data.title}/>
                 <figcaption>
-                    <h2>{this.props.data.title}</h2>
+                    <h2 className="img-title">{this.props.data.title}</h2>
                 </figcaption>
             </figure>
         );
