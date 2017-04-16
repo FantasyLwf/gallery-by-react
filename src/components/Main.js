c2ce3a637d3971a1e4b7ef5b4e66f0815e57f5e9
FantasyLwf <liweifenggs@gmail.com>
2017-04-16 16:43:28 +0800

    change 4.17

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,19 +8,19 @@ let imageDatas = require('../data/imageDatas.json');
 imageDatas = (function genImageURL(imageDatasArr) {
     for (let i = 0; i < imageDatasArr.length; i++) {
         let singleImageData = imageDatasArr[i];
-        singleImageData.imageURL = require('../images/' + singleImageData.filename);
+        singleImageData.imageURL = require('../images/' + singleImageData.fileName);
         imageDatasArr[i] = singleImageData;
     }
     return imageDatasArr;
 })(imageDatas);
 
-class ImgFigure extends React.component {
+class ImgFigure extends React.Component {
     render() {
         return (
-            <figure className="img-figure">
+            <figure className = "img-figure">
                 <img src = {this.props.data.imageURL} alt = {this.props.data.title}/>
                 <figcaption>
-                    <h2 className="img-title">{this.props.data.title}</h2>
+                    <h2 className = "img-title">{this.props.data.title}</h2>
                 </figcaption>
             </figure>
         );
